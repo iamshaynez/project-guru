55b16863953cc221a63ef4e4934c96a26d0922a7
Xiaowen.Z <xiaowen.z@outlook.com>
2024-03-19 13:13:45 +0800

    add:allowed hosts to all

--- project_guru/settings.py
+++ project_guru/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-kf+t_fak$#cl+#51c5^pc_v(x=mzdhj*wg_upcq5n0pxv$@+#(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
